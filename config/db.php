2d703d1c3e87bc5da8c322c17306828c30b47d46
oshchyp <oshchyp.denys@gmail.com>
2018-08-16 14:33:31 +0300

    ewrfwefwef

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ohm_music',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=ohm_music',
     'username' => 'musik',
     'password' => '8T2o0Z2a',
     'charset' => 'utf8',
